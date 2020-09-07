8446e1159dbd1f8bdc77c14d7010192df2fc6611
Ankit Upadhyay <56730118+ankitupadhyay889@users.noreply.github.com>
2020-09-07 15:26:05 +0530

    Second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Home(props) {
-  console.warn("home", props);
+  console.warn("home", props.data);
   return (
     <div>
       <div className="add">
+        <span className="cart-count">{props.data.length}</span>
         <img
-          src="https://previews.123rf.com/images/val2014/val20141603/val2014160300005/54302312-shopping-cart-icon.jpg"
+          src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg"
           alt="cart"
         />
       </div>
